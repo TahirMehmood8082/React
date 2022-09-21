583e1dd177e23e535a18bbf88144ca12a679a458
TahirMehmood8082 <infinitidev31aug22gmail.com>
2022-09-21 20:39:25 +0500

    About US Component with toggle color

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import About from './components/About';
 
 function App() {
   return (
@@ -9,7 +10,8 @@ function App() {
     
 <Navbar title="TextUtilsProps" aboutText="About"/>
 <div className="container my-3">
-  <TextForm heading="Enter the text to analyze below"/>
+  {/* <TextForm heading="Enter the text to analyze below"/> */}
+  <About/>
 
 </div>
 </>
