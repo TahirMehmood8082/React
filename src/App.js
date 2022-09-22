e62cce0ef406f667718171baf65bf275efd89b03
TahirMehmood8082 <infinitidev31aug22gmail.com>
2022-09-22 18:56:15 +0500

    Alert component  Added and stable Code

--- src/App.js
+++ src/App.js
@@ -3,15 +3,43 @@ import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import About from './components/About';
+import Alert from './components/Alert';
+import { useState } from 'react';
 
 function App() {
+  const [mode, setMode] = useState('light')
+  const [alert, setAlert]= useState(null);
+
+  const showAlert = (message, type)=>{
+    setAlert({
+      msg:message,
+      type:type
+    })
+    setTimeout(()=>{
+      setAlert(null);
+    },3000);
+  }
+  const toggleMode = ()=>{
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743'
+      showAlert('Dark Mode enabled', 'success')
+    }
+    else{
+      setMode('light')
+      document.body.style.backgroundColor = 'white'
+      showAlert('Light Mode enabled', 'success')
+    }
+  }
   return (
     <>
     
-<Navbar title="TextUtilsProps" aboutText="About"/>
+<Navbar title="TextUtilsProps" mode={mode} toggleMode={toggleMode}/>
+<Alert alert={alert}/>
 <div className="container my-3">
-  <TextForm heading="Enter the text to analyze below"/>
+  <TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode}/>
   {/* <About/> */}
+  
 
 </div>
 </>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -7,10 +7,12 @@ export default function TextForm(props) {
     const handleUpClick = ()=>{
         console.log('upper btn clicked')
         setText(text.toUpperCase())
+        props.showAlert("converted to uppercase", "success")
     }
     const handleLoClick = ()=>{
         console.log('upper btn clicked')
         setText(text.toLowerCase())
+        props.showAlert("converted to lowercase", "success")
     }
     const handleClear = ()=>{
         console.log('upper btn clicked')
@@ -37,10 +39,10 @@ export default function TextForm(props) {
     const [text, setText] = useState('Enter Text here');
   return (
     <>
-    <div className='container'>
+    <div className='container' style={{color: props.mode==='dark'?'white':'#042743'}}>
         <h1>{props.heading}</h1>
         <div className="mb-3">
-        <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+        <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'grey':'white',color: props.mode==='dark'?'white':'#042743'}} id="myBox" rows="8"></textarea>
         <button className="btn btn-primary my-2 mx-2" onClick={handleUpClick}>convert to Uppercase</button>
         <button className="btn btn-primary my-2 mx-2" onClick={handleLoClick}>convert to Lowercase</button>
         <button className="btn btn-primary my-2 mx-2" onClick={handleClear}>clear</button>
@@ -49,12 +51,12 @@ export default function TextForm(props) {
         </div>
 
     </div>
-    <div className="container my-3">
+    <div className="container my-3" style={{color: props.mode==='dark'?'white':'#042743'}}>
         <h2>Your text summary</h2>
         <p>{text.split(' ').length} words and {text.length} characters</p>
         <p>{0.008 * text.split(" ").length} Minutes read</p>
         <h2>Preview</h2>
-        <p>{text}</p>
+        <p>{text.length>0?text:"Enter text to preview"}</p>
     </div>
     </>
   )
