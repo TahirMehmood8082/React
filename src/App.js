adaf163bf306dfb169e2d14def03277e2501fb7f
TahirMehmood8082 <infinitidev31aug22gmail.com>
2022-09-23 10:16:55 +0500

    index.html:content and title text for SEO plus issues fix of TextUtils

--- src/App.js
+++ src/App.js
@@ -9,8 +9,7 @@ import React from 'react';
 import {
   BrowserRouter as Router,
   Routes,
-  Route,
-  Link
+  Route
 } from "react-router-dom";
 
 function App() {
@@ -31,13 +30,13 @@ function App() {
       setMode('dark');
       document.body.style.backgroundColor = '#042743'
       showAlert('Dark Mode enabled', 'success')
-      document.title = 'TextUtils - Dark Mode'
+      // document.title = 'TextUtils - Dark Mode'
     }
     else{
       setMode('light')
       document.body.style.backgroundColor = 'white'
       showAlert('Light Mode enabled', 'success')
-      document.title = 'TextUtils - Light Mode'
+      // document.title = 'TextUtils - Light Mode'
     }
   }
   return (
@@ -47,8 +46,8 @@ function App() {
     <Alert alert={alert}/>
     <div className="container my-3">
       <Routes>
-          <Route exact path='/about' element={<About/>}/>
-          <Route exact path='/' element={<TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode}/>}/>
+          <Route exact path='/about' element={<About mode={mode}/>}/>
+          <Route exact path='/' element={<TextForm showAlert={showAlert} heading="Try TextUtils - word Counter, Character Counter, Remove extra spaces" mode={mode}/>}/>
               
           
       </Routes>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,4 +1,3 @@
-import { toHaveTextContent } from '@testing-library/jest-dom/dist/matchers'
 import React from 'react'
 import {useState} from 'react'
 
@@ -28,6 +27,9 @@ export default function TextForm(props) {
         var text = document.getElementById("myBox");
         text.select();
         navigator.clipboard.writeText(text.value);
+        //when copy text -text not selected
+        document.getSelection().removeAllRanges();
+        props.showAlert("Copied to Clipboard!", "success")
     }
 
     //Credits: Coding wala
@@ -42,21 +44,22 @@ export default function TextForm(props) {
     <div className='container' style={{color: props.mode==='dark'?'white':'#042743'}}>
         <h1>{props.heading}</h1>
         <div className="mb-3">
-        <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'grey':'white',color: props.mode==='dark'?'white':'#042743'}} id="myBox" rows="8"></textarea>
-        <button className="btn btn-primary my-2 mx-2" onClick={handleUpClick}>convert to Uppercase</button>
-        <button className="btn btn-primary my-2 mx-2" onClick={handleLoClick}>convert to Lowercase</button>
-        <button className="btn btn-primary my-2 mx-2" onClick={handleClear}>clear</button>
-        <button className="btn btn-primary my-2 mx-2" onClick={handleCopy}>copy</button>
-        <button className="btn btn-primary my-2 mx-2" onClick={handleExtraSpaces}>extra spaces</button>
+        <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'#13466e':'white',color: props.mode==='dark'?'white':'#042743'}} id="myBox" rows="8"></textarea>
+        <button disabled={text.length===0} className="btn btn-primary my-2 mx-2" onClick={handleUpClick}>convert to Uppercase</button>
+        <button disabled={text.length===0} className="btn btn-primary my-2 mx-2" onClick={handleLoClick}>convert to Lowercase</button>
+        <button disabled={text.length===0} className="btn btn-primary my-2 mx-2" onClick={handleClear}>clear</button>
+        <button disabled={text.length===0} className="btn btn-primary my-2 mx-2" onClick={handleCopy}>copy</button>
+        <button disabled={text.length===0} className="btn btn-primary my-2 mx-2" onClick={handleExtraSpaces}>extra spaces</button>
         </div>
 
     </div>
     <div className="container my-3" style={{color: props.mode==='dark'?'white':'#042743'}}>
         <h2>Your text summary</h2>
-        <p>{text.split(' ').length} words and {text.length} characters</p>
-        <p>{0.008 * text.split(" ").length} Minutes read</p>
+        {/* this filter code stops spaces to be count in words */}
+        <p>{text.split(' ').filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
+        <p>{0.008 * text.split(" ").filter((element)=>{return element.length!==0}).length} Minutes read</p>
         <h2>Preview</h2>
-        <p>{text.length>0?text:"Enter text to preview"}</p>
+        <p>{text.length>0?text:"Nothing to preview!"}</p>
     </div>
     </>
   )
