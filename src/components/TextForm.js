ea9b57673470ce4ac6cd0fe7b7280c490926fab4
TahirMehmood8082 <infinitidev31aug22gmail.com>
2022-09-23 19:50:14 +0500

    Fixing few more Textutils Issues

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -24,11 +24,11 @@ export default function TextForm(props) {
     }
     //Credits: A
     const handleCopy = ()=>{
-        var text = document.getElementById("myBox");
-        text.select();
-        navigator.clipboard.writeText(text.value);
+        // var text = document.getElementById("myBox");
+        // text.select();
+        navigator.clipboard.writeText(text);
         //when copy text -text not selected
-        document.getSelection().removeAllRanges();
+        // document.getSelection().removeAllRanges();
         props.showAlert("Copied to Clipboard!", "success")
     }
 
@@ -56,7 +56,8 @@ export default function TextForm(props) {
     <div className="container my-3" style={{color: props.mode==='dark'?'white':'#042743'}}>
         <h2>Your text summary</h2>
         {/* this filter code stops spaces to be count in words */}
-        <p>{text.split(' ').filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
+        {/* \s is space including new line  */}
+        <p>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
         <p>{0.008 * text.split(" ").filter((element)=>{return element.length!==0}).length} Minutes read</p>
         <h2>Preview</h2>
         <p>{text.length>0?text:"Nothing to preview!"}</p>
